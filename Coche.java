4eb36e7988c4a4165adfaf431c58c2dc0634ee08
Jaraba <fj.jarabajurado@edu.gva.es>
2024-12-18 16:16:12 +0100

    Clase Coche creada

---
+++ Coche.java
@@ -0,0 +1,11 @@
+public class Coche {
+    String matricula;
+    int anyoMat;
+
+    public Coche (String matricula, int anyo){
+        this.matricula = matricula;
+        this.anyoMat = anyo;
+    }
+
+    
+}
